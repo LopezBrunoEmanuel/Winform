dbf0fae031efb1e19098597e329e47e2b90ecd70
LopezBrunoEmanuel <lopezbrunoemanuel@gmail.com>
2023-02-02 15:48:02 -0300

    Se agrega el proyecto

---
+++ segundoejercicioForms/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace segundoejercicioForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtbApellido = new System.Windows.Forms.TextBox();
+            this.txtbNombre = new System.Windows.Forms.TextBox();
+            this.txtbDireccion = new System.Windows.Forms.TextBox();
+            this.txtbResultado = new System.Windows.Forms.TextBox();
+            this.numEdad = new System.Windows.Forms.NumericUpDown();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numEdad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(12, 17);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(59, 13);
+            this.lblApellido.TabIndex = 0;
+            this.lblApellido.Text = "APELLIDO";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(12, 57);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(54, 13);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "NOMBRE";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(12, 94);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(37, 13);
+            this.lblEdad.TabIndex = 2;
+            this.lblEdad.Text = "EDAD";
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(12, 131);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(66, 13);
+            this.lblDireccion.TabIndex = 3;
+            this.lblDireccion.Text = "DIRECCION";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(12, 165);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(73, 13);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "RESULTADO";
+            // 
+            // txtbApellido
+            // 
+            this.txtbApellido.Location = new System.Drawing.Point(90, 10);
+            this.txtbApellido.MaxLength = 40;
+            this.txtbApellido.Name = "txtbApellido";
+            this.txtbApellido.Size = new System.Drawing.Size(407, 20);
+            this.txtbApellido.TabIndex = 0;
+            // 
+            // txtbNombre
+            // 
+            this.txtbNombre.Location = new System.Drawing.Point(90, 50);
+            this.txtbNombre.MaxLength = 40;
+            this.txtbNombre.Name = "txtbNombre";
+            this.txtbNombre.Size = new System.Drawing.Size(406, 20);
+            this.txtbNombre.TabIndex = 1;
+            // 
+            // txtbDireccion
+            // 
+            this.txtbDireccion.Location = new System.Drawing.Point(91, 124);
+            this.txtbDireccion.MaxLength = 40;
+            this.txtbDireccion.Name = "txtbDireccion";
+            this.txtbDireccion.Size = new System.Drawing.Size(406, 20);
+            this.txtbDireccion.TabIndex = 3;
+            // 
+            // txtbResultado
+            // 
+            this.txtbResultado.Enabled = false;
+            this.txtbResultado.Location = new System.Drawing.Point(12, 181);
+            this.txtbResultado.Multiline = true;
+            this.txtbResultado.Name = "txtbResultado";
+            this.txtbResultado.Size = new System.Drawing.Size(484, 89);
+            this.txtbResultado.TabIndex = 9;
+            // 
+            // numEdad
+            // 
+            this.numEdad.Location = new System.Drawing.Point(90, 87);
+            this.numEdad.Name = "numEdad";
+            this.numEdad.Size = new System.Drawing.Size(120, 20);
+            this.numEdad.TabIndex = 2;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.Location = new System.Drawing.Point(140, 295);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(104, 29);
+            this.btnAceptar.TabIndex = 4;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Location = new System.Drawing.Point(281, 295);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(104, 29);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(524, 336);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.numEdad);
+            this.Controls.Add(this.txtbResultado);
+            this.Controls.Add(this.txtbDireccion);
+            this.Controls.Add(this.txtbNombre);
+            this.Controls.Add(this.txtbApellido);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblDireccion);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblApellido);
+            this.MaximumSize = new System.Drawing.Size(540, 375);
+            this.MinimumSize = new System.Drawing.Size(540, 375);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Datos Personales";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numEdad)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtbApellido;
+        private System.Windows.Forms.TextBox txtbNombre;
+        private System.Windows.Forms.TextBox txtbDireccion;
+        private System.Windows.Forms.TextBox txtbResultado;
+        private System.Windows.Forms.NumericUpDown numEdad;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ segundoejercicioForms/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace segundoejercicioForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (txtbApellido.Text == "")
+                txtbApellido.BackColor = Color.Red;
+            else
+                txtbApellido.BackColor = System.Drawing.SystemColors.Control;
+
+            if (txtbNombre.Text == "")
+                txtbNombre.BackColor = Color.Red;
+            else
+                txtbNombre.BackColor = System.Drawing.SystemColors.Control;
+
+            if (numEdad.Text == "")
+                numEdad.BackColor = Color.Red;
+            else
+                numEdad.BackColor = System.Drawing.SystemColors.Control;
+
+            if (txtbDireccion.Text == "")
+                txtbDireccion.BackColor = Color.Red;
+            else
+                txtbDireccion.BackColor = System.Drawing.SystemColors.Control;
+
+            string apellido = txtbApellido.Text;
+            string nombre = txtbNombre.Text;
+            string edad = numEdad.Text;
+            string direccion = txtbDireccion.Text;
+            txtbResultado.Text = "Apeliido y Nombre:  " + apellido.ToUpper() + " , " + nombre.ToUpper() + 
+                Environment.NewLine + " Edad:  " + edad +
+                Environment.NewLine + " Dirección: " + direccion.ToUpper();
+            
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
